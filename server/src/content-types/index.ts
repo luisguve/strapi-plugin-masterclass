895fcbc5a7364ec0789b0fa4cc5b5b09cda7318f
Luis G. Villegas <lvillegas.ex@meatcode.cl>
2025-04-19 21:18:51 -0400

    feat: adjust content types and extend users plugin and mux video upload plugin

--- server/src/content-types/index.ts
+++ server/src/content-types/index.ts
@@ -2,7 +2,6 @@ import mcCategory from './mc-category/schema.json';
 import mcCourse from './mc-course/schema.json';
 import mcLecture from './mc-lecture/schema.json';
 import mcModule from './mc-module/schema.json';
-import mcStudent from './mc-student/schema.json';
 import mcStudentCourse from './mc-student-course/schema.json';
 
 export default {
@@ -10,6 +9,5 @@ export default {
   'mc-course': { schema: mcCourse },
   'mc-lecture': { schema: mcLecture },
   'mc-module': { schema: mcModule },
-  'mc-student': { schema: mcStudent },
   'mc-student-course': { schema: mcStudentCourse },
 };
--- server/src/register.ts
+++ server/src/register.ts
@@ -1,5 +1,31 @@
 import type { Core } from '@strapi/strapi';
 
-const register = ({ strapi }: { strapi: Core.Strapi }) => {};
+const register = ({ strapi }: { strapi: Core.Strapi }) => {
+  // Extend the user model by adding a new field called courses
+  const user = strapi.contentType('plugin::users-permissions.user')  
+  user.attributes = {
+    // Spread previous defined attributes
+    ...user.attributes,
+    // Add new, or override attributes
+    courses: {
+      type: 'relation',
+      relation: 'oneToMany',
+      target: 'plugin::masterclass.mc-student-course',
+      mappedBy: 'student',
+    },
+  };
+
+  // Extend the mux-video-uploader plugin by adding a new field called lecture
+  const muxAsset = strapi.contentType('plugin::mux-video-uploader.mux-asset')
+  muxAsset.attributes = {
+    ...muxAsset.attributes,
+    lecture: {
+      type: 'relation',
+      relation: 'oneToOne',
+      target: 'plugin::masterclass.mc-lecture',
+      mappedBy: 'video',
+    },
+  };
+};
 
 export default register;

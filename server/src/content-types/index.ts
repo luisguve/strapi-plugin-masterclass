a90da4fcc14ebff3a01932d987c809f43c8f9acf
Luis G. Villegas <lvillegas.ex@meatcode.cl>
2025-04-19 20:04:28 -0400

    feat: create content types

--- server/src/content-types/index.ts
+++ server/src/content-types/index.ts
@@ -1,1 +1,15 @@
-export default {};
+import mcCategory from './mc-category/schema.json';
+import mcCourse from './mc-course/schema.json';
+import mcLecture from './mc-lecture/schema.json';
+import mcModule from './mc-module/schema.json';
+import mcStudent from './mc-student/schema.json';
+import mcStudentCourse from './mc-student-course/schema.json';
+
+export default {
+  'mc-category': { schema: mcCategory },
+  'mc-course': { schema: mcCourse },
+  'mc-lecture': { schema: mcLecture },
+  'mc-module': { schema: mcModule },
+  'mc-student': { schema: mcStudent },
+  'mc-student-course': { schema: mcStudentCourse },
+};
